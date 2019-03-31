e7228b2adf1af0d8bb3b65ddecd99be530f6279f
keiti2 <henrique.keiti2@gmail.com>
2019-03-31 11:48:14 -0300

    Teste
    teste

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
 
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
     user: 'root',
-    password:'',
-    database:'spac'
+    password:'root',
+    database:'spac',
+    insecureAuth : true
 
 });
 connection.connect( function(err){ 
     if ( err ) {  
-      console.erro('erro de conexão:  ' + err.stack);
-      retorno ;
+      console.error('erro de conexão:  ' + err.stack);
+//      retorno ;
+      
     }
     console.log('conectado como id  ' + connection . threadId );
   });
@@ -30,13 +32,13 @@ app.use(bodyParser.json());
 //definindo as rotas
 var indexRoute = require("./src/routes/index-routes");
 var clientRoute = require('./src/routes/client-routes');
+var home = require("./src/View/Inicio")
 
 //vincular a aplicação (app) com o motor de rotas
 app.use('/api',indexRoute);
 //Rotas para produtos
 app.use('/client',clientRoute);
-
+app.use('/',home)
 app.listen(port, () => {
     console.log('Server up and running!!!');
-
 });
