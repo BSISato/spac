e34965490cb6ddbb3d073f34491064f0f7fa6c04
Sato <rogeriohsatobsi@univem.edu.br>
2019-04-05 14:37:12 -0300

    insert tab relacionada

--- src/repositories/client-repository.js
+++ src/repositories/client-repository.js
@@ -14,15 +14,24 @@ const connection = mysql.createConnection({
 //POST
 exports.post = async (data) => {
 
-
-    connection.query("INSERT INTO endereco (logradouro,numero,complemento,bairro,cidade,uf) VALUES ('" + data.logradouro + "','" + data.numero + "','" + data.complemento + "','" + data.bairro + "','" + data.cidade + "','" + data.uf + "')"), function (error,results,fields) {
+    connection.query("INSERT INTO endereco (logradouro,numero,complemento,bairro,cidade,uf) VALUES ('" +
+     data.logradouro + "','" + data.numero + "','" + data.complemento + "','" + data.bairro + "','" +
+     data.cidade + "','" + data.uf + "')"), function (error,results,fields) {
         if (error) throw error;
-        console.log(results.insertId);
     };
-   // connection.query("")
-   // connection.query("INSERT INTO pessoa (nome) VALUES ('"+last_insert_id+"')", function(error){
-  //       if(error) throw error;
-  //  });
+    
+    
+    connection.query("SELECT MAX(idEndereco) FROM endereco", function (err, idend) {
+        if (err) throw error;
+        console.log(idend);
+    });
+
+    connection.query("INSERT INTO pessoa (nome,datanascimento,email,senha,telefone,celular,endereco_idendereco) VALUES ('" +
+    data.nome + "','" + data.datanascimento + "','" + data.email + "','" + data.senha + "','" + 
+    data.telefone + "','" + data.celular + "','" + "1" + "')"), function (error,results,fields) {
+        if (error) throw error;
+    };
+  
 }
 
 //getById
